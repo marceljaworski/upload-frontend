166b64372fb0f74dbb0c4050059588398a24640a
Marcelearn <jaworskimarcel@gmail.com>
2023-05-15 10:51:14 +0200

    vite server config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    origin: 'https://3.75.158.163' || 'https://3.125.183.140' || 'https://35.157.117.28',
+    proxy: {
+      '/fileitems': {
+        target: 'https://upload-backend-4avq.onrender.com',
+        changeOrigin: true,
+      },
+      '/uploadfile': {
+        target: 'https://upload-backend-4avq.onrender.com',
+        changeOrigin: true,
+      }
+    }
+  }
 })
